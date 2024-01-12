b89882a0d9ba2945f7b7b68e0690d36ada98c20c
remeessalam <remeesreme4u@gmail.com>
2024-01-12 21:34:00 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="h-[100vh] flex justify-center items-center w-80">
+        <h1 className="font-extrabold text-red-400 bg-yellow-200 ">TODO-APP</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
